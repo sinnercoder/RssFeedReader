8c5896a678ac6b11a64b7b1d6a589bb8a594aeb4
pankaj <pankajjoship2@gmail.com>
2017-09-25 10:28:40 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,5 +1,13 @@
-import feedparser
+#import the feedparser library first
+import feedparser 
+
+#url is any rss feed url of website where you want to read rss fedds of
 url='https://news.google.com/news/rss/?ned=in&hl=en-IN'
+
+
 feed=feedparser.parse(url)
+
+#t print the title of news 
+#you can also print summary and other contents like date and all using that variable like news.summary etc
 for news in feed.entries:
   print(news.title)
